0b215acd7a17616696f3745d2a2be096edda50c6
keosha-w <keosha.watkins@asurion.com>
2021-10-19 16:27:17 -0500

    Initial Upload

---
+++ scripts/database.js
@@ -0,0 +1,98 @@
+const database = {
+    clays: [
+        {
+            id: 1, 
+            clay: "Earthenware", 
+            pricePerPound: 5.03
+        },
+        {
+            id: 2, 
+            clay: "Stoneware", 
+            pricePerPound: 3.89
+        },
+        {
+            id: 3, 
+            clay: "Bentonite", 
+            pricePerPound: 7.41
+        },
+        {
+            id: 4, 
+            clay: "Porcelain", 
+            pricePerPound: 4.96
+        }
+    ], 
+    glazes: [
+        {
+            id: 1, 
+            glaze: "Crackle", 
+            pricePerPint: 12.33, 
+        }, 
+        {
+            id: 2, 
+            glaze: "Matte", 
+            pricePerPint: 11.68, 
+        }, 
+        {
+            id: 3, 
+            glaze: "Sparkly", 
+            pricePerPint: 16.09, 
+        }, 
+    ],
+    kilns: [
+        {
+            id: 1, 
+            kiln: "Electric", 
+            hourlyRate: 3.09
+        },
+        {
+            id: 2, 
+            kiln: "Gas", 
+            hourlyRate: 6.88
+        },
+        {
+            id: 3, 
+            kiln: "Wood", 
+            hourlyRate: 10.55
+        }
+    ], 
+    customers: [],
+    orders: [],
+    shapes: [
+        {
+            id: 1, 
+            shape: "Umbrella", 
+            difficultyLevel: 9
+        },
+        {
+            id: 2, 
+            shape: "Star", 
+            difficultyLevel: 5
+        },
+        {
+            id: 3, 
+            shape: "Unicorn", 
+            difficultyLevel: 7
+        },
+        {
+            id: 4, 
+            shape: "Mug", 
+            difficultyLevel: 2
+        },
+        {
+            id: 5, 
+            shape: "Bowl", 
+            difficultyLevel: 1
+        },
+        {
+            id: 6, 
+            shape: "Vase", 
+            difficultyLevel: 4
+        },
+        {
+            id: 7, 
+            shape: "Paperweight", 
+            difficultyLevel: 1
+        },
+    ],
+    
+}
